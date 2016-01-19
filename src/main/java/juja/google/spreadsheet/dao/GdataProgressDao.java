67e41f57123adda81cde2c6181449833f253cfa7
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-01-19 19:31:57 +0200

    Implemented concrete Dao

--- src/main/java/juja/google/spreadsheet/dao/GdataProgressDao.java
+++ src/main/java/juja/google/spreadsheet/dao/GdataProgressDao.java
@@ -1,25 +1,36 @@
 package juja.google.spreadsheet.dao;
 
+import com.google.gdata.util.ServiceException;
 import juja.domain.dao.ProgressDao;
 import juja.google.spreadsheet.api.SpreadSheetReader;
 
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import static java.util.Arrays.asList;
-
-/**
- * User: viktor
- * Date: 1/19/16
- */
 public class GdataProgressDao implements ProgressDao {
+
+    public static final String CODE_COLUMN_NAME = "Log-код";
+    private SpreadSheetReader spreadSheetReader;
 
     public GdataProgressDao(SpreadSheetReader spreadSheetReader) {
 
+        this.spreadSheetReader = spreadSheetReader;
     }
 
     @Override
     public Set<String> fetchProgressCodes() {
-        return new HashSet<>(asList("+quiz10", "+q"));
+        HashSet<String> progressCodes = new HashSet<>(getColumnValuesFromSpreadsheet());
+        return progressCodes;
+    }
+
+    private List<String> getColumnValuesFromSpreadsheet() {
+        try {
+            return spreadSheetReader.getColumnValues(CODE_COLUMN_NAME);
+        } catch (IOException | ServiceException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/test/java/juja/google/spreadsheet/dao/GdataProgressDaoTest.java
+++ src/test/java/juja/google/spreadsheet/dao/GdataProgressDaoTest.java
@@ -3,11 +3,14 @@ package juja.google.spreadsheet.dao;
 import juja.google.spreadsheet.api.SpreadSheetReader;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -32,5 +35,19 @@ public class GdataProgressDaoTest {
         assertThat(actualCodes.size(), is(2));
         assertThat(actualCodes, hasItem("+q"));
         assertThat(actualCodes, hasItem("+quiz10"));
+    }
+
+    @Test
+    public void fetchEmptyCollection() throws Exception {
+        //Given
+        SpreadSheetReader spreadSheetReader = mock(SpreadSheetReader.class);
+        GdataProgressDao progressDao = new GdataProgressDao(spreadSheetReader);
+        when(spreadSheetReader.getColumnValues("Log-код")).thenReturn(emptyList());
+
+        //When
+        Set<String> actualCodes = progressDao.fetchProgressCodes();
+
+        //Then
+        assertThat(actualCodes, is(empty()));
     }
 }
